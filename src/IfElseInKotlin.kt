977d6fdbe1f936a9953cbed50a7bc3ad30444bbe
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2024-11-20 23:35:18 +0530

    Code Commited

---
+++ src/IfElseInKotlin.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("If else in kotlin\n")
+
+    val age = 23
+    if (age >= 18)
+        println("You can vote")
+    else
+        println("You can't vote")
+
+
+
+}
---
+++ src/WhenInKotlin.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("When in kotlin\n")
+
+    val myname = "Ritik Verma"
+    when(myname){
+        "Hussein" -> println("My name is Hussein")
+        "Jena" -> println("My name is Jena")
+        "Ali" -> println("My name is Ali")
+        "Ritik Verma" -> println("My name is Ritik Verma")
+        else -> println("I don't know my name")
+    }
+}

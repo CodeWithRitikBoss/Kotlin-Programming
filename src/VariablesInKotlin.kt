4da033249adb9a63b4204953c794e1e48feb9b64
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2024-11-20 23:22:56 +0530

    Code Commited

---
+++ src/VariablesInKotlin.kt
@@ -0,0 +1,12 @@
+fun main(){
+    // var variable are mutable it means changeable.
+    // val variable are immutable it means can't changeable.
+    var x: Int = 12
+    println("The value of x is : $x")
+    x = 13
+    println("Now the value of x is : $x")
+
+    val y: Int = 34
+    println("The value of y is : $y")
+    println("The value of y is : "+ y)
+}
